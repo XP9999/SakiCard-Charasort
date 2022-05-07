d15e242bfa8d833a22c76be32e7590dbaba2f1df
XP9999 <89538085+XP9999@users.noreply.github.com>
2022-05-07 06:46:10 +0630

    Update Cards.js

--- src/js/data/Cards.js
+++ src/js/data/Cards.js
@@ -7,205 +7,256 @@ dataSet[dataSetVersion].characterData = [
   {
     name: "Aislinn Wishard",
     img: "W87boSE.png",
+    opts: {}
   },
   {
     name: "Usuzawa Sae",
     img: "F5UZeLj.png",
+    opts: {}
   },
   {
     name: "Amae Koromo",
     img: "PUGxINh.png",
+    opts: {}
   },
   {
     name: "Anetai Toyone",
     img: "iY0OloA.png",
+    opts: {}
   },
   {
     name: "Arakawa Kei",
     img: "Mve6wOk.png",
+    opts: {}
   },
   {
     name: "Atago Hiroe",
     img: "1OFJMih.png",
+    opts: {}
   },
   {
     name: "Atarashi Ako",
     img: "shhmT01.png",
+    opts: {}
   },
   {
     name: "Ezaki Hitomi",
     img: "zIWWZop.png",
+    opts: {}
   },
   {
     name: "Fukuji Mihoko",
     img: "128Fv3B.png",
+    opts: {}
   },
   {
     name: "Hanada Kirame",
     img: "ZpshiE5.png",
+    opts: {}
   },
   {
     name: "Hao Huiyu",
     img: "xSmTWz5.png",
+    opts: {}
   },
   {
     name: "Haramura Nodoka",
     img: "jd4M8sm.png",
+    opts: {}
   },
   {
     name: "Hirose Sumire",
     img: "7rxtsXi.png",
+    opts: {}
   },
   {
     name: "Ikeda Kana",
     img: "rsR9xVS.png",
+    opts: {}
   },
   {
     name: "Inoue Jun",
     img: "ABslYl0.png",
+    opts: {}
   },
   {
     name: "Iwato Kasumi",
     img: "h3SLEy5.png",
+    opts: {}
   },
   {
     name: "Kainou Yoshiko",
     img: "BSnqytL.png",
+    opts: {}
   },
   {
     name: "Kajiki Yumi",
     img: "2kAcyKd.png",
+    opts: {}
   },
   {
     name: "Kakura Kurumi",
     img: "saf4SI4.png",
+    opts: {}
   },
   {
     name: "Kanbara Satomi",
     img: "c30O8r1.png",
+    opts: {}
   },
   {
     name: "Karijuku Tomoe",
     img: "kERJ2Cc.png",
+    opts: {}
   },
   {
     name: "Kataoka Yuuki",
     img: "TyLd1MB.png",
+    opts: {}
   },
   {
     name: "Kosegawa Shiromi",
     img: "IwW3sIt.png",
+    opts: {}
   },
   {
     name: "Kunihiro Hajime",
     img: "8tRSlpD.png",
+    opts: {}
   },
   {
     name: "Mase Yuuko",
     img: "CXP3igj.png",
+    opts: {}
   },
   {
     name: "Matano Seiko",
     img: "3bzSraw.png",
+    opts: {}
   },
   {
     name: "Matsumi Kuro",
     img: "K2SP1Tj.png",
+    opts: {}
   },
   {
     name: "Matsumi Yuu",
     img: "jhcm5Gg.png",
+    opts: {}
   },
   {
     name: "Maya Yukiko",
     img: "7kx6ENd.png",
+    opts: {}
   },
   {
     name: "Megan Devin",
     img: "6TPkqBx.png",
+    opts: {}
   },
   {
     name: "Miyanaga Saki",
     img: "LZopVuR.png",
+    opts: {}
   },
   {
     name: "Miyanaga Teru",
     img: "ZDN2knl.png",
+    opts: {}
   },
   {
     name: "Nanpo Kazue",
     img: "jGoF7ZZ.png",
+    opts: {}
   },
   {
     name: "Nelly Virsaladze",
     img: "BKXg7LN.png",
+    opts: {}
   },
   {
     name: "Onjouji Toki",
     img: "JzeGbzx.png",
+    opts: {}
   },
   {
     name: "Oohoshi Awai",
     img: "mCWxEXX.png",
+    opts: {}
   },
   {
     name: "Ryuumonbuchi Touka",
     img: "A3I8lrr.png",
+    opts: {}
   },
   {
     name: "Sagimori Arata",
     img: "SGQPCJ9.png",
+    opts: {}
   },
   {
     name: "Sawamura Tomoki",
     img: "fwFIH26.png",
+    opts: {}
   },
   {
     name: "Senoo Kaori",
     img: "5EvQ697.png",
+    opts: {}
   },
   {
     name: "Shibuya Takami",
     img: "YYbtakn.png",
+    opts: {}
   },
   {
     name: "Shimizudani Ryuuka",
     img: "mLV0zdE.png",
+    opts: {}
   },
   {
     name: "Shirouzu Mairu/Tsuruda Himeko",
-    img: "8ZBvLnQ.jpeg",
+    img: "8ZBvLnQ.jpng",
+    opts: {}
   },
   {
     name: "Someya Mako",
     img: "7op9fD1.png",
+    opts: {}
   },
   {
     name: "Takakamo Shizuno",
     img: "T9MUNqf.png",
+    opts: {}
   },
   {
     name: "Takei Hisa",
     img: "MxLLGbu.png",
+    opts: {}
   },
   {
     name: "Takimi Haru",
     img: "CVeAWyN.png",
+    opts: {}
   },
   {
     name: "Touyuko Momoko",
     img: "IaVLOtV.png",
+    opts: {}
   },
   {
     name: "Tsujigaito Satoha",
     img: "X9htdKt.png",
+    opts: {}
   },
   {
     name: "Uzutsumi Hatsumi",
     img: "3hFJdFI.png",
+    opts: {}
   },
   {
     name: "Yumeno Maho",
     img: "DWQg2wr.png",
+    opts: {}
   },
 ]
